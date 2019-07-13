13cea21240a9f3bb9a80a0a5f95919914f1df35b
sh0go <hyacherryapricot@gmail.com>
2019-07-13 19:58:19 +0900

    update

---
+++ Assets/unity-opendmx/FTDI_D2XX.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+public class FTDI_D2XX
+{
+    public const byte BITS_8 = 8;
+    public const byte STOP_BITS_2 = 2;
+    public const byte PARITY_NONE = 0;
+    public const UInt16 FLOW_NONE = 0;
+    public const byte PURGE_RX = 1;
+    public const byte PURGE_TX = 2;
+
+
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_Open(UInt32 uiPort, ref uint ftHandle);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_Close(uint ftHandle);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_Read(uint ftHandle, IntPtr lpBuffer, UInt32 dwBytesToRead, ref UInt32 lpdwBytesReturned);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_Write(uint ftHandle, IntPtr lpBuffer, UInt32 dwBytesToRead, ref UInt32 lpdwBytesWritten);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_SetDataCharacteristics(uint ftHandle, byte uWordLength, byte uStopBits, byte uParity);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_SetFlowControl(uint ftHandle, char usFlowControl, byte uXon, byte uXoff);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_GetModemStatus(uint ftHandle, ref UInt32 lpdwModemStatus);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_Purge(uint ftHandle, UInt32 dwMask);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_ClrRts(uint ftHandle);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_SetBreakOn(uint ftHandle);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_SetBreakOff(uint ftHandle);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_GetStatus(uint ftHandle, ref UInt32 lpdwAmountInRxQueue, ref UInt32 lpdwAmountInTxQueue, ref UInt32 lpdwEventStatus);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_ResetDevice(uint ftHandle);
+    [DllImport("FTD2XX.dll")]
+    public static extern FT_STATUS FT_SetDivisor(uint ftHandle, char usDivisor);
+}
+
+/// <summary>
+/// Enumaration containing the varios return status for the DLL functions.
+/// </summary>
+public enum FT_STATUS
+{
+    FT_OK = 0,
+    FT_INVALID_HANDLE,
+    FT_DEVICE_NOT_FOUND,
+    FT_DEVICE_NOT_OPENED,
+    FT_IO_ERROR,
+    FT_INSUFFICIENT_RESOURCES,
+    FT_INVALID_PARAMETER,
+    FT_INVALID_BAUD_RATE,
+    FT_DEVICE_NOT_OPENED_FOR_ERASE,
+    FT_DEVICE_NOT_OPENED_FOR_WRITE,
+    FT_FAILED_TO_WRITE_DEVICE,
+    FT_EEPROM_READ_FAILED,
+    FT_EEPROM_WRITE_FAILED,
+    FT_EEPROM_ERASE_FAILED,
+    FT_EEPROM_NOT_PRESENT,
+    FT_EEPROM_NOT_PROGRAMMED,
+    FT_INVALID_ARGS,
+    FT_OTHER_ERROR
+};
---
+++ Assets/unity-opendmx/OpenDmx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+public class OpenDmx
+{
+	private uint port = 0;
+	private int device_number = -1;
+	
+	
+	public OpenDmx(int device_number)
+	{
+		this.device_number = device_number;
+		open();
+	}
+	
+	private void open()
+	{
+		uint port_ref = 0;
+		int res = 0;
+		
+		res = (int)FTDI_D2XX.FT_Open((UInt32)this.device_number, ref port_ref);
+		checkRes(res);
+		port = port_ref;
+		
+		res = (int)FTDI_D2XX.FT_ResetDevice(port);
+    	checkRes(res);
+    	
+    	res = (int)FTDI_D2XX.FT_SetDivisor(port, (char)12);
+    	checkRes(res);
+    	
+    	res = (int)FTDI_D2XX.FT_SetDataCharacteristics(port, FTDI_D2XX.BITS_8, FTDI_D2XX.STOP_BITS_2, FTDI_D2XX.PARITY_NONE);
+    	checkRes(res);
+    	
+    	res = (int)FTDI_D2XX.FT_SetFlowControl(port, (char)FTDI_D2XX.FLOW_NONE, (byte)0, (byte)0);
+	    checkRes(res);
+	    res = (int)FTDI_D2XX.FT_ClrRts(port);
+	    checkRes(res);
+	    res = (int)FTDI_D2XX.FT_Purge(port, (char)FTDI_D2XX.PURGE_TX);
+	    checkRes(res);
+	    res = (int)FTDI_D2XX.FT_Purge(port, (char)FTDI_D2XX.PURGE_RX);
+	    checkRes(res);
+	}
+	
+	public int close()
+	{
+		return (int)FTDI_D2XX.FT_Close(port);
+	}
+	
+	public int sendData(byte[] data, int len_data) {
+	    /*
+		if (port != 0)
+	    {
+	    	UnityEditor.EditorApplication.isPlaying = false;
+	    }
+	    if(len_data >= 0 && len_data <= 512){
+	    	UnityEditor.EditorApplication.isPlaying = false;
+	    }
+	    */
+	    
+	    FTDI_D2XX.FT_SetBreakOn(port);
+	    FTDI_D2XX.FT_SetBreakOff(port);
+	    
+	    uint written = 0;
+	    
+	    IntPtr ptr1 = Marshal.AllocHGlobal(1);
+	    byte[] code = {0};
+	    Marshal.Copy(code, 0, ptr1, 1);
+	    int res = (int)FTDI_D2XX.FT_Write(port, ptr1, (uint)1, ref written);
+        checkRes(res);
+        IntPtr ptr2 = Marshal.AllocHGlobal((int)len_data);
+	    Marshal.Copy(data, 0, ptr2, (int)len_data);
+        res = (int)FTDI_D2XX.FT_Write(port, ptr2, (uint)len_data, ref written);
+        checkRes(res);
+	    
+	    return res;
+	  }
+	
+	private void checkRes(int res)
+	{
+    	if (res != (int)FT_STATUS.FT_OK) {
+      		//Debug.Log("problem! check FTDI_D2XX.java for error code info. code: " + res);
+    	}
+  	}
+}
